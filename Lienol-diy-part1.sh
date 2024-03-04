dc98845db3c18914abc20b10ebc75e2e9e81af93
Chris <152150297+innmonkey@users.noreply.github.com>
2024-03-04 14:49:43 +0800

    Update Lienol-diy-part1.sh

--- Lienol-diy-part1.sh
+++ Lienol-diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# File name: Lienol-diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
