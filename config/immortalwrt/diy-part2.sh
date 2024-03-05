07fdbcaa65dea6f66cef7bc3274f12ebe1e24a34
Chris <152150297+innmonkey@users.noreply.github.com>
2024-03-05 13:46:05 +0800

    Delete config directory

--- config/immortalwrt/diy-part2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#========================================================================================================================
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
