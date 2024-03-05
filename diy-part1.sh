e5fea783fc56917e28e9beaf3ea057930ca793fa
Chris <152150297+innmonkey@users.noreply.github.com>
2024-03-05 15:56:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default
